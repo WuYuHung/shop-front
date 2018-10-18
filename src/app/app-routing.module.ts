456987bd5293f67dc44b783dc514db84e561c525
marzear <edward.0726@gmail.com>
2018-10-18 20:20:49 +0800

    add components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CartComponent } from './cart/cart.component';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import { NewArrivalsComponent } from './new-arrivals/new-arrivals.component';
+import { ContactComponent } from './contact/contact.component';
+
 
 const routes: Routes = [
   {
+    path: '',
+    component: HomeComponent
+  },
+  {
     path: 'cart',
     component: CartComponent
+  },
+  {
+    path: 'user/:id',
+    component: UserComponent
+  },
+  {
+    path: 'new-arrival',
+    component: NewArrivalsComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,23 @@ import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CartComponent } from './cart/cart.component';
+import { HomoeComponent } from './homoe/homoe.component';
+import { HomeComponent } from './home/home.component';
+import { NewArrivalsComponent } from './new-arrivals/new-arrivals.component';
+import { ContactComponent } from './contact/contact.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     HeaderComponent,
-    CartComponent
+    CartComponent,
+    HomoeComponent,
+    HomeComponent,
+    NewArrivalsComponent,
+    ContactComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
