040381e4c96c45787ca364d03908c8ac9a7daad9
michael071020 <michael071020@gmail.com>
2018-10-21 22:34:12 +0800

    表單完成前完整版

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CartComponent } from './cart/cart.component';
-import { HomoeComponent } from './homoe/homoe.component';
 import { HomeComponent } from './home/home.component';
 import { NewArrivalsComponent } from './new-arrivals/new-arrivals.component';
 import { ContactComponent } from './contact/contact.component';
@@ -18,17 +17,13 @@ import { UserComponent } from './user/user.component';
     FooterComponent,
     HeaderComponent,
     CartComponent,
-    HomoeComponent,
     HomeComponent,
     NewArrivalsComponent,
     ContactComponent,
     UserComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
-  }
+  Ctrl = function($scope) {
+    $scope.skills = [];
 
+    $scope.skill = '';
+    $scope.submit = function() {
+      if (!!$scope.skill) {
+        $scope.skills.push($scope.skill);
+      }
+      $scope.skill = '';
+    };
+  };
 }
