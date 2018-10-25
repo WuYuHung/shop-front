c0b7f761a2ed30e30b390dc7ba622a073146432e
michael071020 <michael071020@gmail.com>
2018-10-25 13:10:17 +0800

    訂閱者可寄信

--- src/assets/js/subscribe-email.js
+++ src/assets/js/subscribe-email.js
@@ -2,22 +2,22 @@
   emailjs.init("user_L2GOIalZWRcmxD4cBRVUF");
 })();
 
-var myform = $("form#myform");
+var myform = $("form#myform1");
 myform.submit(function (event) {
   event.preventDefault();
 
   // Change to your service ID, or keep using the default service
   var service_id = "contact_form_service";
-  var template_id = "contact_form";
+  var template_id = "subscribe_form";
 
-  myform.find("button").text("寄送中...");
+  myform.find("button").text("Sending...");
   emailjs.sendForm(service_id, template_id, myform[0])
     .then(function () {
-      alert("已成功寄出!");
-      myform.find("button").text("送出");
+      alert("訂閱成功!");
+      myform.find("button").text("Send");
     }, function (err) {
-      alert("寄送失敗\r\n Response:\n " + JSON.stringify(err));
-      myform.find("button").text("送出");
+      alert("訂閱失敗!\r\n Response:\n " + JSON.stringify(err));
+      myform.find("button").text("Send");
     });
   return false;
 });
