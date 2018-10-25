bf455d412bb378d30aa3d874ae18da57ac344215
AIHCER <s310060@shsh.tw>
2018-10-25 20:51:54 +0800

    wei zhong hurry up

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { url } from 'inspector';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
+
 export class UserComponent implements OnInit {
-
+  url = location.href;
+  userid = this.url.split('/')[4];
+  code;
+  key = 'Mike is thin';
   constructor() { }
-
   ngOnInit() {
   }
-
+  onclick() {
+    if (this.code === this.key) {
+      alert('pass');
+    }
+    else {
+      alert('get the fuck out');
+    }
+  }
 }
