5e4c9f723c5869139c17011756b5489f0919252e
AIHCER <s310060@shsh.tw>
2018-10-27 00:59:52 +0800

    finish

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,26 +1,83 @@
-import { Component, OnInit } from '@angular/core';
-import { url } from 'inspector';
+import { Component, OnInit } from "@angular/core";
+import { url } from "inspector";
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  selector: "app-user",
+  templateUrl: "./user.component.html",
+  styleUrls: ["./user.component.css"]
 })
-
 export class UserComponent implements OnInit {
   url = location.href;
-  userid = this.url.split('/')[4];
+  userid = this.url.split("/")[4];
   code;
-  key = 'Mike is thin';
-  constructor() { }
-  ngOnInit() {
-  }
+  key = "Mike is thin";
+  cartList: any;
+
+  findIndex = function(id) {
+    var index = -1;
+    this.cartList.forEach(function(item, key) {
+      if (item.id === id) {
+        index = item.id;
+        return;
+      }
+    });
+    return index;
+  };
+
+  findKey = function(id) {
+    var index = -1;
+    this.cartList.forEach(function(item, key) {
+      if (item.id === id) {
+        index = key;
+        return;
+      }
+    });
+    return index;
+  };
+  remove = function(id) {
+    var index = this.findIndex(id);
+    if (index != -1) {
+      this.cartList.splice(this.findKey(id), 1);
+      localStorage.removeItem(index.toString());
+    }
+  };
+
+  constructor() {}
+  ngOnInit() {}
   onclick() {
     if (this.code === this.key) {
-      alert('pass');
+      alert("pass");
+    } else {
+      alert("get the fuck out");
     }
-    else {
-      alert('get the fuck out');
+  }
+  unpaid() {
+    let json = "[";
+    for (let i = 0, len = localStorage.length; i < len; i++) {
+      json += localStorage.getItem(localStorage.key(i)) + "";
+      if (i !== len - 1) {
+        json += ",";
+      }
     }
+    json += "]";
+    console.log(json);
+    this.cartList = JSON.parse(json);
+    this.cartList = this.cartList.filter(t => !t.paid);
+  }
+  unout() {
+    let json = "[";
+    for (let i = 0, len = localStorage.length; i < len; i++) {
+      json += localStorage.getItem(localStorage.key(i)) + "";
+      if (i !== len - 1) {
+        json += ",";
+      }
+    }
+    json += "]";
+    console.log(json);
+    this.cartList = JSON.parse(json);
+    this.cartList = this.cartList.filter(t => t.paid);
+  }
+  undo() {
+    this.cartList = [];
   }
 }
