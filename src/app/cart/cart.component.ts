715c4754fd08bf08766e9571b75fa5b9cf6ea96b
waywayP <scottiechou@gmail.com>
2018-10-26 21:55:28 +0800

    謝謝華ㄍ隻手遮天把shop的東西給搞的到購物車

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { $ } from 'protractor';
 import { forEach } from '@angular/router/src/utils/collection';
 import { identifierModuleUrl } from '@angular/compiler';
+import { JsonPipe } from '@angular/common';
+
+
+
 
 @Component({
   selector: 'app-cart',
@@ -11,45 +15,51 @@ import { identifierModuleUrl } from '@angular/compiler';
 
 export class CartComponent implements OnInit {
   public products: number;
+  cartList: [];
 
-  cartList = [
-    { id: 1000, name: "iphone5s", quantity: 3, price: 4300 },
-    { id: 1001, name: "iphone5", quantity: 30, price: 3300 },
-    { id: 1002, name: "imac", quantity: 3, price: 3000 },
-    { id: 1003, name: "ipad", quantity: 5, price: 6900 }
-  ];
   findIndex = function (id) {
     var index = -1;
     this.cartList.forEach(function (item, key) {
-      if (item.id == id) {
-        index = key;
+      if (item.id === id) {
+        index = item.id;
         return;
       }
     }); return index;
   };
 
-
+  findKey = function (id) {
+    var index = -1;
+    this.cartList.forEach(function (item, key) {
+      if (item.id === id) {
+        index = key;
+        return;
+      }
+    }); return index;
+  };
   remove = function (id) {
     var index = this.findIndex(id);
     if (index != -1) {
-      this.cartList.splice(index, 1);
+
+      this.cartList.splice(this.findKey(id), 1);
+      localStorage.removeItem(index.toString());
     }
   };
   addOne = function (id) {
-    var index = this.findIndex(id);
+    var index = this.findKey(id);
     if (index != -1) {
       this.cartList[index].quantity++;
     }
   };
   reduceOne = function (id) {
-    var index = this.findIndex(id);
+    var index = this.findKey(id);
     if (index != -1) {
       var item = this.cartList[index];
       if (item.quantity > 1) {
         item.quantity--;
       }
       else {
-        var returnKey = confirm("刪除該商品？");
+
+        let returnKey = confirm("刪除該商品？");
         if (returnKey) {
           this.remove(item.id);
         }
@@ -57,22 +67,31 @@ export class CartComponent implements OnInit {
     }
   };
   totalCost = function () {
-    var total = 0;
-    this.cartList.forEach(function (item, key) {
-      total += item.quantity * item.price;
-    });
+    let total = 0;
+    this.cartList.forEach(t => total += t.price * t.quantity);
     return total;
   };
   totalCount() {
-    var count = 0;
-    this.cartList.forEach(function (item, key) {
-      count += item.quantity;
-    });
+    let count = 0;
+    this.cartList.forEach(t => count += t.quantity);
     return count;
-  };
+  }
 
 
-  constructor() { }
+  constructor() {
+    console.log(localStorage.length);
+    let json = '[';
+    for (let i = 0, len = localStorage.length; i < len; i++) {
+
+     json += localStorage.getItem(localStorage.key(i)) + '';
+     if (i !== len - 1) {
+      json += ',';
+     }
+    }
+    json += ']';
+    console.log(json);
+    this.cartList = JSON.parse(json);
+  }
 
   ngOnInit() {
   }
--- src/app/shop-single/shop-single.component.ts
+++ src/app/shop-single/shop-single.component.ts
@@ -20,14 +20,13 @@ export class ShopSingleComponent implements OnInit {
     this.id = this.route.snapshot.params['id'];
     this.product = jsonFile[this.id - 1];
     this.check = localStorage.getItem(this.id.toString()) ? true : false;
-
   }
 
   addData() {
 
     if (!this.check) {
       this.product['paid'] = false;
-      this.product['quantity'] = 0;
+      this.product['quantity'] = 1;
       localStorage.setItem(this.id.toString(), JSON.stringify(this.product));
     } else {
       localStorage.removeItem(this.id.toString());
