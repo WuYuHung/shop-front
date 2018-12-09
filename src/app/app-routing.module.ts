aa56bc0aa89b2af1a608de91820646aa1ee2e534
marzear <edward.0726@gmail.com>
2018-12-10 00:04:40 +0800

    format

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { CartComponent } from './cart/cart.component';
 import { HomeComponent } from './home/home.component';
 import { UserComponent } from './user/user.component';
-import { NewArrivalsComponent } from './new-arrivals/new-arrivals.component';
 import { ContactComponent } from './contact/contact.component';
 import { ShopComponent } from './shop/shop.component';
 import { ShopSingleComponent } from './shop-single/shop-single.component';
@@ -47,10 +46,6 @@ const routes: Routes = [
   {
     path: 'user',
     component: UserComponent
-  },
-  {
-    path: 'new-arrivals',
-    component: NewArrivalsComponent
   },
   {
     path: 'contact',
