cb7b2bb07668b321f662db8a10f8d0525b28b529
marzear <edward.0726@gmail.com>
2018-12-10 00:06:31 +0800

    format

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CartComponent } from './cart/cart.component';
 import { HomeComponent } from './home/home.component';
-import { NewArrivalsComponent } from './new-arrivals/new-arrivals.component';
 import { ContactComponent } from './contact/contact.component';
 import { UserComponent } from './user/user.component';
 import { ShopComponent } from './shop/shop.component';
@@ -32,7 +31,6 @@ import { ChangepassComponent } from './changepass/changepass.component';
     HeaderComponent,
     CartComponent,
     HomeComponent,
-    NewArrivalsComponent,
     ContactComponent,
     UserComponent,
     ShopComponent,
